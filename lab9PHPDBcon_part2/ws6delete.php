601cfaefd0c8c26d171beca1f7312a2cb15b5afa
unknown <s6404062620109@email.kmutnb.ac.th>
2023-09-27 21:48:52 +0700

    First commit Lab9

---
+++ lab9PHPDBcon_part2/ws6delete.php
@@ -0,0 +1,19 @@
+<?php include "connectdb.php" ?>
+<?php
+    if (isset($_GET['username'])) {
+        $username = $_GET['username'];
+    
+        $stmt = $pdo->prepare("DELETE FROM member WHERE username=?");
+    
+        if ($stmt->execute([$username])) {
+            header("location: ws_6.php");
+            //exit(); // Make sure to exit after redirecting
+        } else {
+            // Handle the case where the execution fails
+            echo "Error deleting member.";
+        }
+    } else {
+        // Handle the case where 'username' is not set in the URL
+        echo "No username specified for deletion.";
+    }
+?>
